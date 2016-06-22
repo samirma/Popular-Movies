6668755b457abc1ad2c19f8f1ed502b29d075433
Samir Moreira Antonio <samir.ma@gmail.com>
2016-06-22 17:22:19 -0300

    Parcelable

--- app/src/main/java/com/samir/popularmovies/model/Movie.java
+++ app/src/main/java/com/samir/popularmovies/model/Movie.java
@@ -1,7 +1,39 @@
 package com.samir.popularmovies.model;
 
 
-public class Movie {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
     public String name;
     public String image;
+
+    public Movie(Parcel parcel) {
+        name = parcel.readString();
+        image = parcel.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(image);
+    }
+
+    public final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel parcel) {
+            return new Movie(parcel);
+        }
+
+        @Override
+        public Movie[] newArray(int i) {
+            return new Movie[i];
+        }
+
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 }
